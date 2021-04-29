2d8dd514065aab0908e02473f26660fb566df102
Nestor <jose.combe@ccc.ufcg.edu.br>
2021-04-28 22:04:54 -0300

    tentativa de adição/entender cadastro

--- projeto-haskell/src/Main.hs
+++ projeto-haskell/src/Main.hs
@@ -3,6 +3,29 @@ import CRUDQuiz
 import System.Console.ANSI
 import System.IO
 import Utils.Util
+import Controller.UserController
+
+
+-- Função de cadastro
+Cadastrar :: String -> String -> String -> IO()
+Cadastrar name email password = do
+    clearScreen
+    name <- getLine
+    email <- getLine
+    password <- getLine
+    addUser name email password
+
+Logar :: String -> String -> IO()
+Logar email password = do
+    clearScreen
+    email <- getLine
+    password <- getLine
+    
+    
+    
+
+
+
 
 -- Função principal para executar o sistema
 main :: IO ()
